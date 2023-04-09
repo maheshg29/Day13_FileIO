600d9ba141244c3c4de3779d80f113add6197859
mahesh <gangurdemahesh9@gmail.com>
2023-04-09 23:30:27 +0530

    [mahesh]add program file for welcome greeting

---
+++ Day13_FileIO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to day 13 File IO system");
+        }
+    }
+}
